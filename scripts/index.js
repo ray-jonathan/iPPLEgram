7b593d153030ecc9b68e02d6851890dffbcf386d
Jonathan Ray <jonathanray17@gmail.com>
2019-02-28 11:26:22 -0500

    adds click handler function

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+// JavaScript for iPPLEgram
+
+// When we click a .picture-frame, 
+const targetElements = document.querySelectorAll("[data-target]"); // a `const` is a "constant variable", which means that it cannot be reassigned
+
+// we see an elarged version of the 
+// image inside the .picture-frame
+
+
+function respondToClick() {
+    console.log("hello");
+    console.log("whats good b");
+    console.log("ehh");
+}
+
+targetElements[0].addEventListener("click", respondToClick);
